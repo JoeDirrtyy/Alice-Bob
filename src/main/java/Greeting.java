f419c10973093a803dc6daf62b3e1313ae8781ba
JoeDirrtyy <joeymarkowski96@gmail.com>
2022-09-14 22:03:40 -0400

    learning scanners

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        System.out.print("What's your name?: ");
+       String name = scanner.nextLine();
+       // System.out.println("Hello " + name + "!");
+        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")  ){
+            System.out.println("Welcome " + name);
+        }else{
+            System.out.println("You are not Alice or Bob");
+        }
+}
+
+
+
+
+    }
+
+
