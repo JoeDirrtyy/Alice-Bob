114ea32e6f73b51eb32a7152ca354bb5ef35b6a5
JoeDirrtyy <joeymarkowski96@gmail.com>
2022-09-16 14:23:59 -0400

    updated

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -4,14 +4,17 @@ public class Greeting {
 
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-        System.out.print("What's your name?: ");
-       String name = scanner.nextLine();
-       // System.out.println("Hello " + name + "!");
-        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")  ){
-            System.out.println("Welcome " + name);
-        }else{
-            System.out.println("You are not Alice or Bob");
-        }
+      while(true) {
+          System.out.print("What's your name?: ");
+          String name = scanner.nextLine();
+          // System.out.println("Hello " + name + "!");
+          if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
+              System.out.println("Welcome " + name);
+              break;
+          } else {
+              System.out.println("You are not Alice or Bob");
+          }
+      }
 }
 
 
